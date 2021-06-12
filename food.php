f6833ee6aad5d2773c12ecc0ca539df897a87a78
UBC Student <genkigee@students.cs.ubc.ca>
2021-06-12 13:21:27 -0700

    Update food.php

--- food.php
+++ food.php
@@ -14,10 +14,96 @@
     <li><a href="rooms.php">Rooms</a></li>
 </ul>
 
-<!-- THIS IS WHERE ALL OUR FUNCTIONALITY WILL GO, THIS IS OUR BODY -->
+<!-- THIS IS WHERE ALL OUR FUNCTIONALITY WILL GO, THIS IS OUR BODY 
 <div style="padding:20px;margin-top:30px;background-color:#ffffff;">
     <h1>Query Food Services</h1>
 </div>
+-->
+
+<!-- JOIN QUERY FOR FOODSERVICES -->
+<div style="padding-top:60px;background-color:#ffffff;">
+<section>
+	<article>
+		<p align='left'>
+		<form method="GET" action="food.php" style ='text-alignt:center'>
+			<label for="calculate">Display food logs:</label>
+			<input type="submit" name="DisplayFoodLogs" value="Display">
+			<input type="submit" name="Hide" value="Clear">
+		</form>
+		</p>
+	</article>
+</section>
+
+<?php
+    if (isset($_GET['DisplayFoodLogs'])) {
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbname = "community-centre";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+		
+		if (isset($_GET['DisplayFoodLogs'])) {
+			$sql = "SELECT f.Food_Service_Name, e.Employee_ID, e.Food, e.TimeAte FROM food_services f, eatsemployee e WHERE f.Food_Service_ID=e.Food_Service_ID";
+			$result = $conn->query($sql);
+			if ($result->num_rows > 0) {
+            // output data of each row
+            echo "<table> <tr>
+                    <th> Food Service Name </th>
+					<th> Employee ID </th>
+                    <th> Food </th>
+					<th> Time Ate </th>
+                    </tr>";
+            while($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>". $row["Food_Service_Name"]. "</td>".
+					"<td>". $row["Employee_ID"]. "</td>".
+					"<td>". $row["Food"]. "</td>".
+                    "<td>". $row["TimeAte"]. "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+			} else {
+            echo "0 results";
+			}
+		}
+		
+		$sql = "SELECT f.Food_Service_Name, m.Membership_ID, m.Food, m.TimeAte FROM food_services f, eatsmembership m WHERE f.Food_Service_ID=m.Food_Service_ID;";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            // output data of each row
+            echo "<table> <tr>
+                    <th> Food Service Name </th>
+					<th> Member ID </th>
+                    <th> Food </th>
+					<th> Time Ate </th>
+                    </tr>";
+            while($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>". $row["Food_Service_Name"]. "</td>".
+					"<td>". $row["Membership_ID"]. "</td>".
+					"<td>". $row["Food"]. "</td>".
+                    "<td>". $row["TimeAte"]. "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+			} else {
+            echo "0 results";
+			}
+        $conn->close();
+    }
+?>
+
+
+
+</div>
+
+
 
 <!-- Food Services Table -->
 <div style="padding:20px;margin-top:30px;background-color:#ffffff;">
@@ -156,7 +242,4 @@
         $conn->close();
     ?>
 </div>
-
-
-
 </html>
