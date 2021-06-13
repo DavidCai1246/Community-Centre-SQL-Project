b15c26e3fe56ff1e0e91e40530cd3f832ced655f
UBC Student <genkigee@students.cs.ubc.ca>
2021-06-13 15:34:35 -0700

    Added single script that includes create+insert

---
+++ sql_scripts/create_insert.sql
@@ -0,0 +1,290 @@
+CREATE TABLE Community_Centre(
+Name CHAR(50),
+Location CHAR(50),
+Open_Time Time,
+Close_Time Time,
+PRIMARY KEY(Name)
+);
+
+#Change from Parking_Stall_Has
+CREATE TABLE Parking_Stall(
+Parking_Num INTEGER,
+Duration Real DEFAULT 2.00,
+Type CHAR(20),
+Community_Centre CHAR(50),
+PRIMARY KEY (Parking_Num),
+FOREIGN KEY (Community_Centre) REFERENCES Community_Centre(Name) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+#Change from Equipment_Contains
+CREATE TABLE Equipment(
+Equipment_ID INTEGER,
+Community_Centre CHAR(50) NOT NULL,
+Type CHAR(20),
+In_Use Boolean DEFAULT FALSE,
+PRIMARY KEY (Equipment_ID),
+FOREIGN KEY (Community_Centre) REFERENCES Community_Centre(Name) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+#Change from Memberships_Uses_Parks
+CREATE TABLE Memberships(
+Membership_ID INTEGER,
+Age INTEGER,
+Name CHAR(20),
+Address CHAR(30),
+Equipment_ID INTEGER UNIQUE,
+Parking_Num INTEGER,
+PRIMARY KEY (Membership_ID),
+FOREIGN KEY (Equipment_ID) REFERENCES Equipment(Equipment_ID) ON UPDATE CASCADE,
+FOREIGN KEY (Parking_Num) REFERENCES Parking_Stall(Parking_Num) ON UPDATE CASCADE
+);
+
+CREATE TABLE Employee(
+Employee_ID INTEGER,
+Salary INTEGER,
+Age INTEGER not null,
+Name CHAR(20) not null,
+Type CHAR(20) not null,
+Address CHAR(30),
+Hours INTEGER not null,
+Parking_Num INTEGER,
+Community_Centre CHAR(50) not null,
+PRIMARY KEY (Employee_ID),
+FOREIGN KEY (Parking_Num) REFERENCES Parking_Stall(Parking_Num) ON DELETE CASCADE,
+FOREIGN KEY (Community_Centre) REFERENCES Community_Centre(Name) ON DELETE CASCADE
+);
+
+#Change from Volunteer_WorksIn
+CREATE TABLE Volunteer(
+Employee_ID INTEGER,
+School CHAR(50),
+PRIMARY KEY (Employee_ID),
+FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID) ON DELETE CASCADE
+);
+
+#Change from Staff_WorksIn
+CREATE TABLE Staff(
+Employee_ID INTEGER,
+Role CHAR(30),
+PRIMARY KEY(Employee_ID),
+FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID) ON DELETE CASCADE
+);
+ 
+#Change from Contractor_WorksIn
+CREATE TABLE Contractor(
+Employee_ID INTEGER,
+Contract_Start Date,
+Contract_End Date,
+PRIMARY KEY(Employee_ID),
+FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID) ON DELETE CASCADE
+);
+
+#Change from Food_Services_Contains
+CREATE TABLE Food_Services(
+Food_Service_Name CHAR(50),
+Location CHAR(50),
+Community_Centre CHAR(50),
+Food_Service_ID INTEGER,
+PRIMARY KEY(Food_Service_ID),
+FOREIGN KEY(Community_Centre) REFERENCES Community_Centre(Name) ON DELETE CASCADE
+);
+
+CREATE TABLE EatsEmployee(
+Employee_ID INTEGER,
+Food_Service_ID INTEGER,
+Food CHAR(30),
+TimeAte Date,
+PRIMARY KEY (Employee_ID, Food_Service_ID, TimeAte),
+FOREIGN KEY (Employee_ID) REFERENCES Employee(Employee_ID) ON DELETE CASCADE,
+FOREIGN KEY (Food_Service_ID) REFERENCES Food_Services(Food_Service_ID) ON DELETE CASCADE
+);
+
+CREATE TABLE EatsMembership(
+Membership_ID INTEGER,
+Food_Service_ID INTEGER,
+Food CHAR(30),
+TimeAte Date,
+PRIMARY KEY(Food_Service_ID, Membership_ID, Food, TimeAte),
+FOREIGN KEY(Membership_ID) REFERENCES Memberships(Membership_ID) ON DELETE CASCADE,
+FOREIGN KEY(Food_Service_ID) REFERENCES Food_Services(Food_Service_ID) ON DELETE CASCADE
+);
+
+CREATE TABLE Room(
+Type CHAR(20),
+Size INTEGER NOT NULL,
+Community_Centre CHAR(50) NOT NULL,
+Is_Reserved Boolean,
+Room_ID INTEGER,
+ReservedBy INTEGER,
+PRIMARY KEY (Room_ID),
+FOREIGN KEY (Community_Centre) REFERENCES Community_Centre(Name) ON DELETE CASCADE ON UPDATE CASCADE,
+FOREIGN KEY (ReservedBy) REFERENCES Staff(Employee_ID) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+#Change from Events_HeldIn_OrganizedBy
+CREATE TABLE Events(
+Date_of_Event Date,
+Event_ID INTEGER,
+Room_ID INTEGER,
+Employee_ID INTEGER,
+PRIMARY KEY (Event_ID),
+FOREIGN KEY (Room_ID) REFERENCES Room(Room_ID) ON DELETE CASCADE,
+FOREIGN KEY (Employee_ID) REFERENCES Staff(Employee_ID) ON DELETE CASCADE
+);
+
+CREATE TABLE Fitness_Classes(
+Event_ID INTEGER,
+Instructor_Name CHAR(10),
+PRIMARY KEY(Event_ID),
+FOREIGN KEY (Event_ID) REFERENCES Events(Event_ID) ON DELETE CASCADE
+);
+
+CREATE TABLE Intramural_League (
+Event_ID INTEGER,
+Sports_Type CHAR(20),
+PRIMARY KEY(Event_ID),
+FOREIGN KEY (Event_ID) REFERENCES Events(Event_ID) ON DELETE CASCADE
+);
+
+CREATE TABLE Community_Outreach (
+Event_ID INTEGER,
+Attendance INTEGER,
+PRIMARY KEY(Event_ID),
+FOREIGN KEY (Event_ID) REFERENCES Events(Event_ID) ON DELETE CASCADE
+);
+
+CREATE TABLE Attends(
+Membership_ID INTEGER,
+Event_ID INTEGER,
+PRIMARY KEY(Membership_ID,Event_ID),
+FOREIGN KEY (Membership_ID) REFERENCES Memberships(Membership_ID) ON DELETE CASCADE,
+FOREIGN KEY (Event_ID) REFERENCES Events(Event_ID) ON DELETE CASCADE
+);
+
+CREATE TABLE Sponsor_Funds (
+Name CHAR(20),
+Amount REAL,
+Event_ID INTEGER,
+PRIMARY KEY (Name, Event_ID),
+FOREIGN KEY (Event_ID) REFERENCES Events(Event_ID) ON DELETE CASCADE
+);
+
+
+INSERT INTO Community_Centre
+VALUES ('Vancouver Community Centre', 'Vancouver Road', '08:00:00', '17:00:00'),
+('Surrey Community Centre', 'Surrey Road', '08:00:00', '17:00:00'),
+('Coquitlam Community Centre', 'Coquitlam Road', '08:00:00', '17:00:00'),
+('Richmond Community Centre', 'Richmond Road', '08:00:00', '17:00:00'),
+('Port Moody Community Centre', 'Port Moody Road', '08:00:00', '17:00:00');
+
+INSERT INTO Parking_Stall
+VALUES (999, 2.0, 'Regular', 'Vancouver Community Centre'),
+(998, 2.0, 'Regular', 'Vancouver Community Centre'),
+(997, 2.0, 'Regular', 'Vancouver Community Centre'),
+(996, 2.0, 'Regular', 'Vancouver Community Centre'),
+(995, 3.0, 'Accessible', 'Vancouver Community Centre'),
+(100, 3.0, 'Regular', 'Vancouver Community Centre'),
+(101, 3.0, 'Regular', 'Vancouver Community Centre'),
+(102, 3.0, 'Regular', 'Vancouver Community Centre'),
+(103, 3.0, 'Regular', 'Vancouver Community Centre'),
+(104, 3.0, 'Accessible', 'Vancouver Community Centre'),
+(450, 4.0, 'Regular', 'Vancouver Community Centre'),
+(451, 4.0, 'Regular', 'Vancouver Community Centre'),
+(452, 4.0, 'Regular', 'Vancouver Community Centre'),
+(453, 4.0, 'Regular', 'Vancouver Community Centre'),
+(454, 4.0, 'Accessible', 'Vancouver Community Centre'),
+(333, 0.5, 'Regular', 'Vancouver Community Centre'),
+(334, 0.5, 'Regular', 'Vancouver Community Centre');
+
+INSERT INTO Equipment
+VALUES (889, 'Vancouver Community Centre', 'Basketball', TRUE),
+(888, 'Vancouver Community Centre', 'Soccer Ball', TRUE),
+(887, 'Vancouver Community Centre', 'Volley Ball', TRUE),
+(886, 'Vancouver Community Centre', 'Badminton Racket', TRUE),
+(885, 'Vancouver Community Centre', 'Tennis Ball', TRUE);
+
+INSERT INTO Memberships
+VALUES (1, 12, 'Timmy Member', 'Timmy Road', 889, null),
+(2, 13, 'Jimmy Member', 'Jimmy Road', 888, null),
+(3, 16, 'Kimmy Member', 'Kimmy Road', null, 999),
+(4, 17, 'Rimmy Member', 'Rimmy Road', 886, 999),
+(5, 32, 'Shimmy Member', 'Shimmy Road', null, 999);
+
+INSERT INTO Employee
+VALUES (11, 20, 23, 'Jeff Staff', 'Staff', 'Jeff Road', 40, 998, 'Vancouver Community Centre'),
+(12, 22, 25, 'Sam Contractor', 'Contractor', 'Sam Road', 10, 997, 'Vancouver Community Centre'),
+(13, 25, 32, 'Sarah Staff', 'Staff', 'Sarah Road', 40, 996, 'Vancouver Community Centre'),
+(14, 0, 18, 'Ben Volunteer', 'Volunteer', 'Ben Road', 5, null, 'Vancouver Community Centre'),
+(15, 0, 16, 'Den Volunteer', 'Volunteer', 'Den Road', 3, null, 'Vancouver Community Centre');
+
+INSERT INTO Volunteer
+VALUES (14, 'SFU'),
+(15, 'UBC');
+
+INSERT INTO Staff
+VALUES (11, 'Camp Leader'),
+(13, 'Manager');
+
+INSERT INTO Contractor
+VALUES (12, '2021-01-01', '2021-09-01');
+
+INSERT INTO Food_Services
+VALUES ('Mcdonalds', 'By the pool', 'Vancouver Community Centre', 779),
+('Burger King', 'First Floor', 'Vancouver Community Centre', 778),
+('A&W', 'Second Floor', 'Vancouver Community Centre', 777),
+('Harveys Hashbrowns', 'Third Floor', 'Vancouver Community Centre', 776),
+('Genkis Deli', 'Roof', 'Vancouver Community Centre', 775);
+
+INSERT INTO EatsEmployee
+VALUES (11, 779, 'Burger', '2021-03-01'),
+(12, 778, 'Fries', '2021-03-02'),
+(13, 778, 'Hashbrown', '2021-03-02'),
+(14, 775, 'Turkey Meat', '2021-03-01'),
+(15, 775, 'Chicken', '2021-03-01');
+
+INSERT INTO EatsMembership
+VALUES (1, 779, 'Burger', '2021-03-01'),
+(1, 779, 'Fries', '2021-03-01'),
+(1, 777, 'Burger', '2021-03-01'),
+(5, 776, 'Burger', '2021-03-01'),
+(5, 775, 'Burger', '2021-03-01');
+
+INSERT INTO Room
+VALUES ('Weight Room',900,'Vancouver Community Centre',TRUE,101,13),
+('Studio Room 1',500,'Vancouver Community Centre',TRUE,102,11),
+('Offic',400,'Vancouver Community Centre',FALSE,103,NULL),
+('Studio Room 2',500,'Vancouver Community Centre',TRUE,201,13),
+('Gymnasium A',5000,'Vancouver Community Centre',TRUE,104,11),
+('Gymnasium B',5500,'Vancouver Community Centre',TRUE,105,13),
+('Multi Purpose Room',2000,'Vancouver Community Centre',TRUE,106,13);
+
+INSERT INTO Events
+VALUES ('2021-03-01',1001,101,13),
+('2021-03-01',1002,102,11),
+('2021-03-01',1003,201,13),
+('2021-03-01',1004,104,11),
+('2021-03-01',1005,105,13),
+('2021-03-01',1006,106,13);
+
+INSERT INTO Fitness_Classes
+VALUES (1001,"Chad"),
+(1002,"David"),
+(1003,"Kendra");
+
+INSERT INTO Intramural_League
+VALUES (1004,"Basketball"),
+(1005,"Volleyball");
+
+INSERT INTO Community_Outreach
+VALUES (1006,100);
+
+INSERT INTO Attends
+VALUES (1,1002),
+(2,1004),
+(3,1005),
+(4,1001),
+(5,1003),
+(5,1006);
+
+INSERT INTO Sponsor_Funds
+VALUES ("Bill Gatts",10000,1006);
